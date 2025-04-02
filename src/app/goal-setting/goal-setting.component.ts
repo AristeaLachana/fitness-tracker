f04f50c44903b612de84d405888de4315ac70174
AristeaLachana <alachana@csd.auth.gr>
2025-04-02 15:51:21 +0300

    Header v1 styled

---
+++ src/app/goal-setting/goal-setting.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-setting',
+  imports: [],
+  templateUrl: './goal-setting.component.html',
+  styleUrl: './goal-setting.component.css'
+})
+export class GoalSettingComponent {
+
+}
---
+++ src/app/workout-log/workout-log.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-log',
+  imports: [],
+  templateUrl: './workout-log.component.html',
+  styleUrl: './workout-log.component.css'
+})
+export class WorkoutLogComponent {
+
+}
