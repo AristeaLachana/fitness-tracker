4e816f5f8c828e3473bbcdac21bbc9960f08f88b
AristeaLachana <alachana@csd.auth.gr>
2025-04-02 19:22:35 +0300

    Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WorkoutService } from '../workout.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  totalWorkouts: number = 0;
+  totalCalories: number = 0;
+  goalsAchieved: number = 0;
+  averageDuration: number = 0;
 
+  constructor(private workoutService: WorkoutService){}
+
+  ngOnInit() {
+    this.workoutService.getWorkouts().subscribe(workouts => {
+      this.totalWorkouts = workouts.length;
+      this.totalCalories = workouts.reduce((sum, workout) => sum + workout.calories, 0);
+      this.goalsAchieved = workouts.filter(workout => workout.goalAchieved).length;
+      this.averageDuration = workouts.length ? workouts.reduce((sum, workout) => sum + workout.duration, 0) / workouts.length : 0;
+    });
+  }
 }
--- src/app/workout.service.ts
+++ src/app/workout.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface Workout {
+  id: number;
+  type: string;
+  duration: number;
+  calories: number;
+  goalAchieved: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkoutService {
+  private workouts: Workout[] = [
+    {id: 1, type: 'Running', duration: 30, calories: 300, goalAchieved: true},
+    {id: 2, type: 'Cycling', duration: 45, calories: 400, goalAchieved: false},
+    {id: 3, type: 'Swimming', duration: 30, calories: 250, goalAchieved: true}
+  ];
 
   constructor() { }
+  getWorkouts(): Observable<Workout[]> {
+    return of(this.workouts);
+  }
 }
