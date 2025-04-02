9b47eaa9ebd74d6163f0ac42d01beb329b7e2ca0
AristeaLachana <alachana@csd.auth.gr>
2025-04-02 20:09:26 +0300

    Goal Setting

--- src/app/goal-setting/goal-setting.component.ts
+++ src/app/goal-setting/goal-setting.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Goal {
+  id: number;
+  type: string;
+  targetValue: number;
+  deadline: Date;
+  achieved: boolean;
+}
 
 @Component({
   selector: 'app-goal-setting',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './goal-setting.component.html',
   styleUrl: './goal-setting.component.css'
 })
 export class GoalSettingComponent {
+  goals: Goal[] = [];
+  goalType: string = '';
+  targetValue: number | null = null;
+  deadline: Date | null = null;
+  goalSaved: boolean = false;
 
+  addGoal() {
+    if (this.goalType && this.targetValue && this.deadline) {
+      const newGoal: Goal = {
+        id: this.goals.length + 1,
+        type: this.goalType,
+        targetValue: this.targetValue,
+        deadline: this.deadline,
+        achieved: false
+      };
+      this.goals.push(newGoal);
+      this.goalSaved = true;
+      this.resetForm();
+    }
+  }
+
+  resetForm() {
+    this.goalType = '';
+    this.targetValue = null;
+    this.deadline = null;
+  }
 }
