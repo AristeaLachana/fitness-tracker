d9673304f0942aa1ec5aff8f8de3257dd45c66a8
AristeaLachana <alachana@csd.auth.gr>
2025-04-03 11:05:34 +0300

    Workout Log

--- src/app/progress-chart/progress-chart.component.ts
+++ src/app/progress-chart/progress-chart.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-progress-chart',
   imports: [],
+  standalone: true,
   templateUrl: './progress-chart.component.html',
   styleUrl: './progress-chart.component.css'
 })
-export class ProgressChartComponent {
+export class ProgressChartComponent implements OnInit {
+  chartData: ChartData<'line'> = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Calories Burnt',
+        data: [],
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        fill: true,
+      }
+    ]
+  };
 
+  chartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
+  };
+
+  ngOnInit() {
+    this.loadChartData();
+  }
+
+  loadChartData() {
+    //data !!!!!!!!!!
+  }
 }
--- src/app/workout-log/workout-log.component.ts
+++ src/app/workout-log/workout-log.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Workout { 
+  id: number;
+  type: string;
+  duration: number;
+  calories: number;
+  date: Date;
+}
 
 @Component({
   selector: 'app-workout-log',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './workout-log.component.html',
   styleUrl: './workout-log.component.css'
 })
 export class WorkoutLogComponent {
+  workouts: Workout[] = [];
+  workoutType: string = '';
+  duration: number | null = null;
+  calories: number | null = null;
+  date: Date | null = null;
+  workoutSaved: boolean = false;
 
+  logWorkout() {
+    if (this.workoutType && this.duration && this.calories && this.date){
+      const newWorkout: Workout = {
+        id: this.workouts.length + 1,
+        type: this.workoutType,
+        duration: this.duration,
+        calories: this.calories,
+        date: this.date
+      };
+      this.workouts.push(newWorkout);
+      this.workoutSaved = true;
+      this.resetForm()
+    }
+  }
+
+  resetForm() {
+    this.workoutType = '',
+    this.duration = null,
+    this.calories = null,
+    this.date = null;
+  }
 }
